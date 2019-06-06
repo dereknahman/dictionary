da50ffc040e84a77d8e08cfe536d76e42890a4f7
kirsty-simmonds <kirstysimmonds@icloud.com>
2019-06-06 17:28:11 +0200

    refined output

--- dictionary.py
+++ dictionary.py
@@ -20,4 +20,11 @@ def translate(word):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
