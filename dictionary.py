80c7595aa27a2be75e617994fdc8ef7b13cfcbf7
kirsty-simmonds <kirstysimmonds@icloud.com>
2019-06-06 17:21:33 +0200

    first commit

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            "I don't understand your query."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+
+print(translate(word))
